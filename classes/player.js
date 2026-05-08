ec39256bc12a72f3b17a33dcfdfcf2c29e82ef79
AdamMakay <makayadam1@gmail.com>
2026-05-08 20:49:45 +0200

    Movement,Board

--- classes/player.js
+++ classes/player.js
@@ -1,3 +1,32 @@
-export default class Player{
-    
+
+
+
+
+export class Player{
+    constructor(x,y){
+        this.x = x
+        this.y = y
+        this.speed = 7   
+    }
+
+    CreatePlayer(ctx){
+        ctx.fillStyle = "blue"
+        ctx.fillRect(this.x,this.y,20,20)
+    }
+
+    ChangeDirection(event){
+        const key = event.key
+
+        if(key == "ArrowUp"){
+            this.y -= this.speed
+        }else if(key == "ArrowDown"){
+            this.y += this.speed
+        }else if(key == "ArrowRight"){
+            this.x += this.speed
+        }else if(key == "ArrowLeft"){
+            this.x -= this.speed
+        }
+
+    }
+
 }
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-//meg nem kezdtem el
+import { Board } from './classes/board.js';
+import { Player } from './classes/player.js';
+
+
+const board = new Board()
+const player = new Player(100, 100)
+
+
+function HandleKeyDown(event){
+    player.ChangeDirection(event)
+    updateGame()
+}
+
+ function updateGame(){
+    board.ctx.clearRect(0, 0, board.gameBoard.width, board.gameBoard.height);
+    player.CreatePlayer(board.ctx)
+ }
+window.addEventListener("keydown",HandleKeyDown)
+
+player.CreatePlayer(board.ctx)
